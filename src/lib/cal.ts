d846824a477b3aa63413fe8fbeebaebc5aa042b9
Usama <usama@Usamas-MacBook-Air.local>
2026-06-22 19:12:04 +0500

    fix(cal): remove .validator to resolve SSR build error

--- src/lib/cal.ts
+++ src/lib/cal.ts
@@ -4,7 +4,6 @@ const CAL_API_KEY = "cal_live_ad25597eb1b5d86451ff91803b49dc96";
 const EVENT_TYPE_ID = "5912238"; // Free 15 min call with Katie
 
 export const getAvailableSlots = createServerFn({ method: 'GET' })
-  .validator((data: { startDate: string; endDate: string }) => data)
   .handler(async ({ data }) => {
     try {
       const url = `https://api.cal.com/v2/slots/available?eventTypeId=${EVENT_TYPE_ID}&startTime=${data.startDate}&endTime=${data.endDate}`;
